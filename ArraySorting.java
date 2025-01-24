2c50423dea4d35bd2c78b89c8c8102566149b984
Ramya Lanka <ramya.lanka@example.com>
2025-01-24 20:20:39 +0530

    Add ArraySorting.java file

---
+++ ArraySorting.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 5, 6};
+
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+}
